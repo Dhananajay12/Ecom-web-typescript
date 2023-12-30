d8d3bdd4d3b6af1899568a76b9edc34a5d8b320e
DhananjayPatilkaki <dhananjay@patilkaki.com>
2023-12-30 16:05:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+			<p className="bg-slate-100 shadow-md text-2xl rounded-md px-4 py-3">
+				Hello world!
+			</p>
+    </>
+  )
+}
+
+export default App
